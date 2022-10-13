9233c56a93a521db1d02dc78e4a929e71a902c02
Plamen Georgiev <plamen.flame@gmail.com>
2022-10-13 13:27:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [test, setTest] = useState("Not loaded");
+  useEffect(() => {
+    const fetchFn = async () => {
+      const response = await fetch("http://3.16.23.82:8080/test");
+      setTest(response.data);
+      console.log(response.body);
+    };
+    fetchFn();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {test}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +28,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>Test variable: {process.env.REACT_APP_TEST_VARIABLE}</div>
       </header>
     </div>
   );
